1e7ad772bf4972e55d97845edb2f19ff3213b2c7
lice <496078344@qq.com>
2018-05-25 23:55:03 +0800

    lb

--- pages/user/collection/collection.js
+++ pages/user/collection/collection.js
@@ -1,11 +1,61 @@
 // pages/user/collection/collection.js
+import config from '../../../config/constant.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    list: [
+      {
+        img: '/assets/user/collection-0.png',
+        title: '企业文化',
+        content: '近日，全国首场外汇市场准则培训在北京中国银行总行举办。中国银行副行长任德奇，外管局国际收支司司长.....',
+        select: false,
+        data: [{
+          value: 56,
+          name: 'users'
+        }, {
+          value: 36,
+          name: 'like'
+        }, {
+          value: 16,
+          name: 'wechat'
+        }]
+      }, {
+        img: '/assets/user/collection-1.png',
+        title: '规则制度',
+        content: '近日，全国首场外汇市场准则培训在北京中国银行总行举办。中国银行副行长任德奇，外管局国际收支司司长.....',
+        select: false,
+        data: [{
+          value: 56,
+          name: 'users'
+        }, {
+          value: 36,
+          name: 'like'
+        }, {
+          value: 16,
+          name: 'wechat'
+        }]
+      }, {
+        img: '/assets/user/collection-2.png',
+        title: '法律法规',
+        content: '近日，全国首场外汇市场准则培训在北京中国银行总行举办。中国银行副行长任德奇，外管局国际收支司司长.....',
+        select: false,
+        data: [{
+          value: 56,
+          name: 'users'
+        }, {
+          value: 36,
+          name: 'like'
+        }, {
+          value: 16,
+          name: 'wechat'
+        }]
+      }, 
+    ],
+    canEdit: false,
+    imgSrc: config.imgSrc
   },
 
   /**
@@ -28,39 +78,16 @@ Page({
   onShow: function () {
   
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  changeEditStatus(){
+    this.setData({
+      canEdit: !this.data.canEdit
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  select(e){
+    let index = e.currentTarget.dataset.index;
+    let attr = 'list[' + index + ']' + '.select';
+    this.setData({
+      [attr]: !this.data.list[index].select
+    })
   }
 })
